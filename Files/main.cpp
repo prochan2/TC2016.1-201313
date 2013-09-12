812a4890ad5ad6cccbb5b84b09e0d3b6b0ae9f8e
Antonín Procházka <prochan2@fit.cvut.cz>
2013-09-11 20:42:06 -0500

    Files commented

--- Files/main.cpp
+++ Files/main.cpp
@@ -8,15 +8,17 @@ int main(int argc, char** argv) {
     ifstream file;
     // You've got to put here a path to an existing text file
     // Working with text files:
-    // - on Windows use Notepad
+    // - on Windows use Notepad, backslashes in the path have to be doubled! (\\)
     // - on OSX (Mac) use TextEdit and use menu Format > Make Plain Text (not RTF)
     // - on Linux use gedit (on Gnome) or kedit (on KDE)
     file.open("/Users/alumno/NetBeansProjects/files/textfile.txt");
     
-    string line;
+    // This way you can read the file word by word.
+    // To see how to process the file by lines, have a look at slides TC2016.1-Lesson9-C++practice
+    string word;
     while (file.good()) {
-        file >> line;
-        cout << line << " ";
+        file >> word;
+        cout << word << " ";
     }
     
     // Don't forget to close the file in the end!
